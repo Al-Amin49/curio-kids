393e83a679572a355fd731b4f5f01c3c5300c11c
Al-Amin49 <alamin496832@gmail.com>
2024-08-27 01:47:10 +0600

    initial setup done

---
+++ src/app/components/Shared/Foooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Foooter = () => {
+    return (
+        <div>
+            <h3>THis is footer</h3>
+        </div>
+    );
+};
+
+export default Foooter;
---
+++ src/app/components/UI/Home/Banner.tsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <h3 className="text-3xl">THis is home</h3>
+        </div>
+    );
+};
+
+export default Banner;
