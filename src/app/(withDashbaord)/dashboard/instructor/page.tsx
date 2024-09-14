9acfc94e14c06d41808bfc8d10e00093bb314150
Al-Amin49 <alamin496832@gmail.com>
2024-09-14 12:31:53 +0600

    instructor page added

--- src/app/(withDashbaord)/dashboard/instructor/page.tsx
+++ src/app/(withDashbaord)/dashboard/instructor/page.tsx
@@ -1,12 +1,14 @@
+'use client'
+import { useAuth } from '@/lib/AuthProvider';
 import React from 'react';
-
 const InstructorPage = () => {
-    return (
-        <div>
-            <h3>Instructor page</h3>
-        </div>
-    );
+  const {user}= useAuth();
+  return (
+    <div className='flex flex-col justify-center items-center '>
+      
+      <h3 className='text-2xl text-black font-bold'>Welcome Back, Instructor<span className='text-primary'> {user?.name}</span> </h3>
+    </div>
+  );
 };
 
-export default InstructorPage
-;
+export default InstructorPage;
