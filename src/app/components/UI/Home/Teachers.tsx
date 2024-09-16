4328178af9099dde26e9af1478ae9121f8750098
Al-Amin49 <alamin496832@gmail.com>
2024-09-16 20:52:19 +0600

    fetch teacher data from backend api

--- src/app/components/UI/Home/Teachers.tsx
+++ src/app/components/UI/Home/Teachers.tsx
@@ -2,9 +2,18 @@ import { teachersData } from "@/app/constants/data";
 import Container from "../Container";
 import teacherBg from "@/assets/shape/teacher-bg.jpg";
 import Image from "next/image";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
 
-const Teachers = () => {
+const Teachers = async () => {
+  const res = await fetch(`https://curio-kids-server.vercel.app/instructors`, {
+    cache: "no-store",
+  });
+  const instructors = await res.json();
   return (
     <div
       className="h-full py-10  bg-cover bg-top bg-no-repeat"
@@ -17,42 +26,57 @@ const Teachers = () => {
       </h3>
       <Container>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mx-4 lg:mx-0">
-          {teachersData.slice(0,4).map((teacher) => (
+          {instructors.slice(0, 4).map((teacher) => (
             <div
-              key={teacher.id}
+              key={teacher._id}
               className="relative rounded-lg p-6 flex flex-col items-center text-center "
             >
               {/* Teacher Image */}
-              <div className="relative w-40 h-40 mb-6"
-              
-              >
+              <div className="relative w-40 h-40 mb-6">
                 <Image
-                  src={teacher.image}
+                  src={teacher.profilePicture}
                   alt={teacher.name}
                   className="rounded-full object-cover w-full h-full"
                   width={260}
                   height={260}
-                 
                 />
                 {/* Social Media Icons */}
                 <div className="absolute inset-0 flex items-center justify-end space-x-4 opacity-0 hover:opacity-100 transition-opacity duration-300">
-                  <a href={teacher.socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={teacher.socialLinks.linkedin}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FaLinkedinIn className="text-primary w-6 h-6 hover:text-secondary transition-colors duration-200" />
                   </a>
-                  <a href={teacher.socialLinks.facebook} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={teacher.socialLinks.facebook}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FaFacebookF className="text-primary w-6 h-6 hover:text-secondary transition-colors duration-200" />
                   </a>
-                  <a href={teacher.socialLinks.twitter} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={teacher.socialLinks.twitter}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FaTwitter className="text-primary w-6 h-6 hover:text-secondary transition-colors duration-200" />
                   </a>
-                  <a href={teacher.socialLinks.instagram} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={teacher.socialLinks.instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <FaInstagram className="text-primary w-6 h-6 hover:text-secondary transition-colors duration-200" />
                   </a>
                 </div>
               </div>
 
               {/* Teacher Name & Designation */}
-              <h4 className="text-lg font-semibold text-black mb-2">{teacher.name}</h4>
+              <h4 className="text-lg font-semibold text-black mb-2">
+                {teacher.name}
+              </h4>
               <p className="text-gray-600">{teacher.designation}</p>
             </div>
           ))}
